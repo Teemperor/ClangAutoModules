0e42bc8dafd81786a77916b66547ffccd484afbd
Raphael Isemann <teemperor@gmail.com>
2017-07-19 16:47:51 +0200

    Enabled AppleClang for building PCMs

--- ClangModules.cmake
+++ ClangModules.cmake
@@ -87,7 +87,7 @@ function(ClangModules_SetupSTL)
   endif()
 endfunction()
 
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
   message(STATUS "Configuring ClangModules")
   #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ")
   #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} )
