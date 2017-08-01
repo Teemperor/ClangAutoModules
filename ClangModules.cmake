61b75cde8864864d9c9a3f9620bc03b4cb430f2c
Raphael Isemann <teemperor@gmail.com>
2017-08-01 11:47:10 +0200

    Now using one modulemap per include dir and one VFS in total.
    This of course means we now have to merge modulemaps and incrementally
    increase our VFS, which made the code a bit more complicated than
    it was in the beginning, but now we can finally mount multiple modules
    in the same include path!

--- ClangModules.cmake
+++ ClangModules.cmake
@@ -77,19 +77,12 @@ endfunction()
 
 function(ClangModules_MountModulemap)
   set(options)
-  set(oneValueArgs RESULT VFS PATH NEW_FLAGS MODULEMAP MODULES CXX_FLAGS)
+  set(oneValueArgs RESULT TARGET_MODULEMAP PATH MODULEMAP MODULES CXX_FLAGS)
   set(multiValueArgs)
   cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
   if (ARG_UNPARSED_ARGUMENTS)
     message(ERROR "Unparsed args: ${ARG_UNPARSED_ARGUMENTS}")
   endif()
-
-  get_filename_component(VFS_FILENAME "${ARG_MODULEMAP}" NAME)
-  set(OUTPUT_VFS "${CMAKE_BINARY_DIR}/ClangModules_${VFS_FILENAME}")
-
-  set(PATH_PLACEHOLDER "${ARG_PATH}")
-  set(MODULEMAP_PLACEHOLDER "${ARG_MODULEMAP}")
-  configure_file("${ARG_VFS}" "${OUTPUT_VFS}" @ONLY)
 
   ClangModules_CheckHeadersExist(MODULEMAP "${ARG_MODULEMAP}"
                                  PATH "${ARG_PATH}" 
@@ -99,13 +92,18 @@ function(ClangModules_MountModulemap)
   ClangModules_GetHeadersFromModulemap(RESULT headers MODULEMAP ${ARG_MODULEMAP})
 
   if(headers_exist)
-    set(new_args " -fmodule-map-file=${ARG_PATH}/module.modulemap -ivfsoverlay${OUTPUT_VFS} ")
-    string(MD5 ARG_HASH "${ARG_CXX_FLAGS} ${new_args}")
-    string(SUBSTRING ${ARG_HASH} 0 8 ARG_HASH)
-    get_filename_component(ModuleName "${ARG_MODULEMAP}" NAME_WE)
+    file(READ "${ARG_MODULEMAP}" modulemap_contents)
+    set(TARGET_EXISTS NO)
+    if (EXISTS "${ARG_TARGET_MODULEMAP}")
+      set(TARGET_EXISTS YES)
+      execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${ARG_TARGET_MODULEMAP}" "${ARG_TARGET_MODULEMAP}.tmp")
+    endif()
 
-    set(tmp_cache_path "${CMAKE_BINARY_DIR}/ClangModules_TmpPCMS_${ARG_HASH}")
-    set(CMAKE_REQUIRED_FLAGS "${ARG_CXX_FLAGS} ${new_args} -fmodules-cache-path=${tmp_cache_path}")
+    file(APPEND "${ARG_TARGET_MODULEMAP}" "\n${modulemap_contents}\n")
+
+    get_filename_component(ModuleName "${ARG_MODULEMAP}" NAME_WE)
+    set(tmp_cache_path "${CMAKE_BINARY_DIR}/ClangModules_TmpPCMS_${ModuleName}")
+    set(CMAKE_REQUIRED_FLAGS "${ARG_CXX_FLAGS} -fmodules-cache-path=${tmp_cache_path}")
     include(CheckCXXSourceCompiles)
 
     set(INCLUDE_LIST)
@@ -127,28 +125,38 @@ function(ClangModules_MountModulemap)
         endif()
       endforeach()
       if(FOUND_PCMS)
-        set(${ARG_NEW_FLAGS} "${ARG_CXX_FLAGS} ${new_args}" PARENT_SCOPE)
-        set(${ARG_RESULT} YES PARENT_SCOPE)
+        message(STATUS "Clang was able to compile module!")
+        set(TMP_RESULT YES)
       else()
         message(STATUS "Clang ignored modulemap for ${ModuleName}. Skipping")
-        set(${ARG_RESULT} NO PARENT_SCOPE)
+        set(TMP_RESULT NO)
       endif()
     else()
       message(STATUS "Failed to compile '${tmp_cache_path}'")
-      set(${ARG_RESULT} NO PARENT_SCOPE)
+      set(TMP_RESULT NO)
     endif()
     if(EXISTS "${tmp_cache_path}")
       file(REMOVE_RECURSE "${tmp_cache_path}")
     endif()
+
+    # Restore original modulemap on failure
+    if (NOT TMP_RESULT)
+      if(TARGET_EXISTS)
+        execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${ARG_TARGET_MODULEMAP}.tmp" "${ARG_TARGET_MODULEMAP}")
+      else()
+        file(REMOVE "${ARG_TARGET_MODULEMAP}")
+      endif()
+    endif()
   else()
-    message(STATUS "Couldn't find headers ${missing_headers} in ${ARG_PATH}")
-    set(${ARG_RESULT} NO PARENT_SCOPE)
+    #message(STATUS "Couldn't find headers ${missing_headers} in ${ARG_PATH}")
+    set(TMP_RESULT NO)
   endif()
+  set(${ARG_RESULT} ${TMP_RESULT} PARENT_SCOPE)
 endfunction()
 
 function(ClangModules_SetupModulemaps)
   set(options)
-  set(oneValueArgs CXX_FLAGS)
+  set(oneValueArgs CXX_FLAGS VFS NEW_FLAGS)
   set(multiValueArgs)
   cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
   if (ARG_UNPARSED_ARGUMENTS)
@@ -176,71 +184,128 @@ function(ClangModules_SetupModulemaps)
     endif()
   endforeach()
 
+  set(new_flags "")
+
+  set(modulemap_id 1)
+
+  set(first_iter YES)
   foreach(INCLUDE_PATH ${INCLUDE_LIST})
     message(STATUS "Testing: ${INCLUDE_PATH}")
+
+
+    math(EXPR modulemap_id "${modulemap_id}+1")
+    set(final_modulemap_path "${CMAKE_BINARY_DIR}/ClangModules-${modulemap_id}.modulemap")
+
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${ARG_VFS}" "${ARG_VFS}.tmp")
+    if(NOT first_iter)
+      file(APPEND ${ARG_VFS} ",\n")
+    endif()
+    file(APPEND ${ARG_VFS} "  { 'name': '${INCLUDE_PATH}', 'type': 'directory',\n")
+    file(APPEND ${ARG_VFS} "    'contents':\n")
+    file(APPEND ${ARG_VFS} "      [{ 'name': 'module.modulemap', 'type': 'file',\n")
+    file(APPEND ${ARG_VFS} "        'external-contents': '${final_modulemap_path}'\n")
+    file(APPEND ${ARG_VFS} "      }]\n")
+    file(APPEND ${ARG_VFS} "  }\n")
+
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${ARG_VFS}" "${ARG_VFS}.unfinished")
+    #finalize VFS
+    file(APPEND ${ARG_VFS} "]}\n")
+
+    set(first_iter NO)
+
+    set(test_new_flag "-fmodule-map-file=${INCLUDE_PATH}/module.modulemap")
+    set(final_test_flags "${ARG_CXX_FLAGS} ${test_new_flag}")
+
+    set(SUCCESS NO)
+    set(TMP_SUCCESS NO)
+
     if(NOT STL_SUCCESS)
-    ClangModules_MountModulemap(VFS "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl.yaml"
+    ClangModules_MountModulemap(TARGET_MODULEMAP "${final_modulemap_path}"
                                 PATH "${INCLUDE_PATH}"
                                 MODULEMAP "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl17.modulemap"
                                 MODULES stl17
-                                CXX_FLAGS "${ARG_CXX_FLAGS}"
-                                NEW_FLAGS NEW_CXX_FLAGS
-                                RESULT STL_SUCCESS)
+                                CXX_FLAGS "${final_test_flags}"
+                                RESULT TMP_SUCCESS)
+      if(TMP_SUCCESS)
+        set(SUCCESS "YES")
+        set(STL_SUCCESS "YES")
+      endif()
     endif()
     if(NOT STL_SUCCESS)
-    ClangModules_MountModulemap(VFS "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl.yaml"
+    ClangModules_MountModulemap(TARGET_MODULEMAP "${final_modulemap_path}"
                                 PATH "${INCLUDE_PATH}"
                                 MODULEMAP "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl14.modulemap"
                                 MODULES stl14
-                                CXX_FLAGS "${ARG_CXX_FLAGS}"
-                                NEW_FLAGS NEW_CXX_FLAGS
-                                RESULT STL_SUCCESS)
+                                CXX_FLAGS "${final_test_flags}"
+                                RESULT TMP_SUCCESS)
+      if(TMP_SUCCESS)
+        set(SUCCESS "YES")
+        set(STL_SUCCESS "YES")
+      endif()
     endif()
     if(NOT STL_SUCCESS)
-    ClangModules_MountModulemap(VFS "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl.yaml"
+    ClangModules_MountModulemap(TARGET_MODULEMAP "${final_modulemap_path}"
                                 PATH "${INCLUDE_PATH}"
                                 MODULEMAP "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl11.modulemap"
                                 MODULES stl11
-                                CXX_FLAGS "${ARG_CXX_FLAGS}"
-                                NEW_FLAGS NEW_CXX_FLAGS
-                                RESULT STL_SUCCESS)
+                                CXX_FLAGS "${final_test_flags}"
+                                RESULT TMP_SUCCESS)
+      if(TMP_SUCCESS)
+        set(SUCCESS "YES")
+        set(STL_SUCCESS "YES")
+      endif()
     endif()
     if(NOT STL_SUCCESS)
-    ClangModules_MountModulemap(VFS "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl.yaml"
+    ClangModules_MountModulemap(TARGET_MODULEMAP "${final_modulemap_path}"
                                 PATH "${INCLUDE_PATH}"
                                 MODULEMAP "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/stl03.modulemap"
                                 MODULES stl03
-                                CXX_FLAGS "${ARG_CXX_FLAGS}"
-                                NEW_FLAGS NEW_CXX_FLAGS
-                                RESULT STL_SUCCESS)
+                                CXX_FLAGS "${final_test_flags}"
+                                RESULT TMP_SUCCESS)
+      if(TMP_SUCCESS)
+        set(SUCCESS "YES")
+        set(STL_SUCCESS "YES")
+      endif()
     endif()
     if(NOT SDL2_SUCCESS)
-    ClangModules_MountModulemap(VFS "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/sdl2.yaml"
+    ClangModules_MountModulemap(TARGET_MODULEMAP "${final_modulemap_path}"
                                 PATH "${INCLUDE_PATH}"
                                 MODULEMAP "${CMAKE_CURRENT_SOURCE_DIR}/clang-modules/files/sdl2.modulemap"
                                 MODULES sdl2
-                                CXX_FLAGS "${ARG_CXX_FLAGS}"
-                                NEW_FLAGS NEW_SDL2_FLAGS
-                                RESULT SDL2_SUCCESS)
+                                CXX_FLAGS "${final_test_flags}"
+                                RESULT TMP_SUCCESS)
+      if(TMP_SUCCESS)
+        set(SUCCESS "YES")
+        set(SDL2_SUCCESS "YES")
+      endif()
+    endif()
+
+    if(SUCCESS)
+      set(new_flags "${new_flags} ${test_new_flag}")
+      execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${ARG_VFS}.unfinished" "${ARG_VFS}")
+    else()
+      execute_process(COMMAND ${CMAKE_COMMAND} -E copy "${ARG_VFS}.tmp" "${ARG_VFS}")
     endif()
   endforeach()
-
-
-  set(NEW_CXX_FLAGS "${NEW_CXX_FLAGS} ${NEW_SDL2_FLAGS}" PARENT_SCOPE)
+  set(${ARG_NEW_FLAGS} "${new_flags}" PARENT_SCOPE)
 endfunction()
 
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
   message(STATUS "Configuring ClangModules")
 
+  set(vfs_file "${CMAKE_BINARY_DIR}/ClangModulesVFS.yaml")
+  file(WRITE ${vfs_file} "{ 'version': 0, 'roots': [\n")
   get_property(current_compile_options DIRECTORY PROPERTY COMPILE_OPTIONS)
-  set(CXX_FLAGS "${CMAKE_CXX_FLAGS} ${current_compile_options} -fmodules -fcxx-modules -fno-implicit-module-maps")
+  set(CXX_FLAGS "${CMAKE_CXX_FLAGS} ${current_compile_options} -fmodules -fcxx-modules ")
+  set(CXX_FLAGS "${CXX_FLAGS} -fno-implicit-module-maps -ivfsoverlay${vfs_file}")
 
   set(LANG_BAK $ENV{LANG})
   set(ENV{LANG} C)
 
-  ClangModules_SetupModulemaps(CXX_FLAGS "${CXX_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${NEW_CXX_FLAGS} -fmodules-cache-path=${CMAKE_BINARY_DIR}/pcms")
+  ClangModules_SetupModulemaps(CXX_FLAGS "${CXX_FLAGS}" VFS "${vfs_file}" NEW_FLAGS ClangModules_NewFlags)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_FLAGS} ${ClangModules_NewFlags} -fmodules-cache-path=${CMAKE_BINARY_DIR}/pcms")
 
+  file(APPEND ${vfs_file} "]}\n")
   set(ENV{LANG} ${LANG_BAK})
 
 endif()
