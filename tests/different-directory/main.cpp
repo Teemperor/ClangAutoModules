543d3cea36e75a627f61ce283ca45e36f4a0fb74
Raphael Isemann <teemperor@gmail.com>
2017-08-01 17:50:42 +0200

    Make the script independent of where it is placed.
    Right now we depenend on being in the folder cmake-modules,
    this removes this hardcoded path.

---
+++ tests/different-directory/main.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+int main(int argc, char **argv) {
+  std::vector<int> f;
+  std::pair<int, int> p;
+  std::string str;
+  std::stack<int> sta;
+  std::queue<double> qu;
+  std::map<int, int> mm;
+  std::list<float> lf;
+  std::set<int> sset;
+  double fcos = std::cos(0);
+}
