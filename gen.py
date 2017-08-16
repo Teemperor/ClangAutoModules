b4939afc9e4cd60247937d4384fe323403cba128
Raphael Isemann <teemperor@gmail.com>
2017-08-16 16:36:06 +0200

    Now using CTest to run project tests

--- gen.py
+++ gen.py
@@ -2,19 +2,23 @@
 
 import os
 
-unpack_code=""
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
-for modulemap_name in os.listdir('files'):
-  with open('files/' + modulemap_name, 'r') as modulemap:
+unpack_code = ""
+
+for modulemap_name in os.listdir(script_dir + '/files'):
+  with open(script_dir + '/files/' + modulemap_name, 'r') as modulemap:
     data=modulemap.read().replace("##UNPACK_PLACEHOLDER", unpack_code)
     data = data.replace("\"", "\\\"")
     unpack_code += "file(WRITE \"${ClangModules_UNPACK_FOLDER}/" + modulemap_name + "\" \""
     unpack_code += data
     unpack_code += "\")\n\n"
 
+if len(unpack_code) == 0:
+  print("Couldn't find /files directory! Script dir is " + script_dir)
+  exit(1)
 
-
-with open('ClangModules.in.cmake', 'r') as infile:
+with open(script_dir + '/ClangModules.in.cmake', 'r') as infile:
     data = infile.read()
     data = data.replace("##UNPACK_PLACEHOLDER", unpack_code)
     with open('ClangModules.cmake', 'w') as outfile:
--- run_tests.sh
+++ run_tests.sh
@@ -1,109 +1,15 @@
 #!/bin/bash
+set -e
 
-skip_checks="NO"
 export CC="$1"
 export CXX="$2"
-
-if [ "$3" == "NO" ]; then
-  echo "Disabling PCM checks. Only testing for compilation..."
-  skip_checks="YES"
-fi
-
-declare -a errors
-
-function run_test {
-  set -e
-  start_dir="$(pwd)"
-  test_dir="$1"
-  build_dir="$2"
-  source_dir="$3"
-  
-  cd "$build_dir"
-  test_name=`basename "$1"`
-  echo "- Running test: $test_name"
-  
-  # Copy our test directory to the tmp directory
-  rm -rf "$test_name"
-  mkdir "$test_name"
-  cd "$test_name"
-  cp -r "$test_dir"/* .
-
-  # Setup the clang-modules files in test_dir/clang-modules
-  mkdir clang-modules
-  cd clang-modules
-  cp    "$source_dir"/ClangModules.cmake .
-  
-
-  # Build the project
-  cd ..
-  mkdir build
-  cd build
-  FLAGS="-H"
-  if [[ $CXX == *"clang"* ]]; then
-    FLAGS="$FLAGS -Rmodule-build"
-  fi
-  CXX_FLAGS="-H"
-  if [[ $CXX == *"clang"* ]]; then
-    CXX_FLAGS="$CXX_FLAGS -Rmodule-build"
-  fi
-
-  set +e
-  cmake -DCMAKE_C_FLAGS="$FLAGS"  -DCMAKE_CXX_FLAGS="$CXX_FLAGS" ..
-  if [ $? -ne 0 ]; then
-    echo "CMake failed!"
-    errors+=("$test_name -> CMake error")
-  fi
-  make VERBOSE=1
-  if [ $? -ne 0 ]; then
-    echo "make failed!"
-    errors+=("$test_name -> make error")
-  fi
-  
-  if [ "$skip_checks" == "NO" ] ; then
-    while read p; do
-      # Verify that we used the right modules
-      find ./pcms/ -name "$p-*\\.pcm" | grep -q .
-
-      if [ $? -ne 0 ]; then
-        errors+=("$test_name -> can't find $p")
-        had_fail="TRUE"
-        echo "ERROR: $test_name -> can't find PCM for $p"
-        echo "PWD: $(pwd)"
-        cd ..
-        echo "TREE:"
-        tree
-        cd build
-        echo "YAML:"
-        cat ClangModules_*.yaml
-        echo "CMakeFiles/CMakeError.log"
-        cat CMakeFiles/CMakeError.log
-        echo "CMakeFiles/CMakeOutput.log"
-        cat CMakeFiles/CMakeOutput.log
-      else
-        echo "-- Found PCM for $p: $(find . -name "$p-*\\.pcm")"
-      fi
-    done < ../NEEDS_PCMS
-  fi
-  cd "$start_dir"
-}
+shift
+shift
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-echo "Generating..."
-pwd_bak="$(pwd)"
 cd "$DIR"
-$DIR/gen.py
-cd "$pwd_bak"
-
-for test_dir in $DIR/tests/*
-do
-  run_test "$test_dir" `pwd` "$DIR"
-done
-
-if [ ${#errors[@]} -eq 0 ]; then
-    echo "No errors. All tests passed."
-else
-    echo "Tests failed:"
-    printf '%s\n' "${errors[@]}"
-    exit 1
-fi
+rm -rf build
+mkdir build
+cd build
+cmake ..
+ctest --output-on-failure $@
