0efda2133d049d2809fe0f459d03a89bf89ccf61
Raphael Isemann <teemperor@gmail.com>
2017-09-25 16:04:29 +0200

    Rewrote the backend in Python

--- gen.py
+++ gen.py
@@ -6,12 +6,21 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 
 unpack_code = ""
 
+def escape_string_for_cmake(data):
+  return data.replace("\\", "\\\\").replace("\"", "\\\"")
+
+with open(script_dir + '/ClangModules.py', 'r') as python_script:
+  data = python_script.read()
+  unpack_code += "#Write the python backend out:\n"
+  unpack_code += "file(WRITE \"${ClangModules_UNPACK_FOLDER}/ClangModules.py\" \""
+  unpack_code += escape_string_for_cmake(data)
+  unpack_code += "\")\n\n"
+  
 for modulemap_name in os.listdir(script_dir + '/files'):
   with open(script_dir + '/files/' + modulemap_name, 'r') as modulemap:
-    data=modulemap.read().replace("##UNPACK_PLACEHOLDER", unpack_code)
-    data = data.replace("\"", "\\\"")
+    data = modulemap.read()
     unpack_code += "file(WRITE \"${ClangModules_UNPACK_FOLDER}/" + modulemap_name + "\" \""
-    unpack_code += data
+    unpack_code += escape_string_for_cmake(data)
     unpack_code += "\")\n\n"
 
 if len(unpack_code) == 0:
