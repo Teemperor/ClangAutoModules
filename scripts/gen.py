10e4f75a752c91d19b1cdac264f5eac1aefb467c
Raphael Isemann <teemperor@gmail.com>
2017-09-29 16:03:17 +0200

    Move util scripts to own dir

--- scripts/gen.py
+++ scripts/gen.py
@@ -9,15 +9,15 @@ unpack_code = ""
 def escape_string_for_cmake(data):
   return data.replace("\\", "\\\\").replace("\"", "\\\"")
 
-with open(script_dir + '/ClangModules.py', 'r') as python_script:
+with open(script_dir + '/../ClangModules.py', 'r') as python_script:
   data = python_script.read()
   unpack_code += "#Write the python backend out:\n"
   unpack_code += "file(WRITE \"${ClangModules_UNPACK_FOLDER}/ClangModules.py\" \""
   unpack_code += escape_string_for_cmake(data)
   unpack_code += "\")\n\n"
   
-for modulemap_name in os.listdir(script_dir + '/files'):
-  with open(script_dir + '/files/' + modulemap_name, 'r') as modulemap:
+for modulemap_name in os.listdir(script_dir + '/../files'):
+  with open(script_dir + '/../files/' + modulemap_name, 'r') as modulemap:
     data = modulemap.read()
     unpack_code += "file(WRITE \"${ClangModules_UNPACK_FOLDER}/" + modulemap_name + "\" \""
     unpack_code += escape_string_for_cmake(data)
@@ -27,7 +27,7 @@ if len(unpack_code) == 0:
   print("Couldn't find /files directory! Script dir is " + script_dir)
   exit(1)
 
-with open(script_dir + '/ClangModules.in.cmake', 'r') as infile:
+with open(script_dir + '/../ClangModules.in.cmake', 'r') as infile:
     data = infile.read()
     data = data.replace("##UNPACK_PLACEHOLDER", unpack_code)
     with open('ClangModules.cmake', 'w') as outfile:
